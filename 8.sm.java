4e8235c89d2217213d58e153a590e785fadbee17
nicorobin0508 <117674705+nicorobin0508@users.noreply.github.com>
2023-10-02 21:22:31 +0530

    Update 8.sm.java

--- 8.sm.java
+++ 8.sm.java
@@ -1,46 +1,62 @@
 import 'package:flutter/material.dart';
+
 void main() {
- runApp(MaterialApp(
- home: Home(),
- ));
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
+
 class Home extends StatefulWidget {
- const Home({super.key});
- @override
- State<Home> createState() => _HomeState();
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
 }
+
 class _HomeState extends State<Home> {
- double result=0;
- final TextEditingController first=TextEditingController();
- final TextEditingController second=TextEditingController();
- @override
- Widget build(BuildContext context) {
- return Scaffold(
- appBar: AppBar(
- title: Text('State Management',),
- ),
- body: Container(
- child: Column(
- children: [
- TextField(controller: first,
- decoration: InputDecoration(hintText: 'enter first no',),),
- TextField(controller: second,
- decoration: InputDecoration(hintText: 'enter second no',),),
- Row(
- children: [
- ElevatedButton(onPressed: (){
- double a=double.parse(first.text);
- double b=double.parse(second.text);
- setState(() {
- result=a*b;
- });
- }, child: Text('click here'),),
- ],
- ),
- Text('result $result'),
- ],
- ),
- ),
- );
- }
+  double result = 0;
+  final TextEditingController first = TextEditingController();
+  final TextEditingController second = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('State Management'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            TextField(
+              controller: first,
+              decoration: InputDecoration(
+                hintText: 'enter first no',
+              ),
+            ),
+            TextField(
+              controller: second,
+              decoration: InputDecoration(
+                hintText: 'enter second no',
+              ),
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    double a = double.parse(first.text);
+                    double b = double.parse(second.text);
+                    setState(() {
+                      result = a * b;
+                    });
+                  },
+                  child: Text('click here'),
+                ),
+              ],
+            ),
+            Text('result $result'),
+          ],
+        ),
+      ),
+    );
+  }
 }
